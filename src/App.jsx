8d433df6f8cd131a4bcb095a4b115feabeaec57a
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2024-07-27 21:43:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-// import React from 'react';
+
+// import React, { useState } from 'react';
+// import { useState } from 'react';
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ContactSection from './ContactSection.jsx'
@@ -8,49 +10,47 @@ import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import {faComment} from '@fortawesome/free-solid-svg-icons';
 import {faBell} from '@fortawesome/free-solid-svg-icons';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
+import Profile from './Profile.jsx';
+
 function App() {
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [userProfile, setUserProfile] = useState({
+  //   name: 'Ankit Singh',
+  //   university: 'Guru Jambheshwar University of Science & Technology',
+  //   course: 'CSE(A&MI)',
+  //   year: '3rd Year contact',
+  // });
+
+  // function changeval() {
+  //   setIsEditing(true);
+  // }
+
+  // function handleInputChange(event) {
+  //   const { name, value } = event.target;
+  //   setUserProfile({ ...userProfile, [name]: value });
+  // }
+
+  // function handleSave() {
+  //   setIsEditing(false);
+  // }
+
   return (
     <div>
-    <div className="container">
-      {/* <div className="header">
-        <div className="header-left">
-          <i className="fa fa-home"></i>
-          <a href="https://figma.com/proto/d0rc">figma.com/proto/d0rc</a>
+      <div className="container">
+        <div className="sidebar">
+          <ul>
+            <li><FontAwesomeIcon icon={faList} /> All Products</li>
+            <li><FontAwesomeIcon icon={faCartShopping} /> Cart</li>
+            <li><FontAwesomeIcon icon={faComment} /> Messages</li>
+            <li><FontAwesomeIcon icon={faBell} /> Notification</li>
+            <li><FontAwesomeIcon icon={faUser} /> Profile</li>
+            <li><FontAwesomeIcon icon={faPhone} /><a id="anchor" href="#contact-us">Contact Us</a></li>
+          </ul>
         </div>
-        <div className="header-right">
-          <i className="fa fa-plus"></i>
-          <i className="fa fa-comments"></i>
-          <i className="fa fa-ellipsis-v"></i>
-        </div>
-      </div> */}
-      <div className="sidebar">
-        <ul>
-          <li><FontAwesomeIcon icon={faList} /> All Products</li>
-          <li><FontAwesomeIcon icon={faCartShopping} /> Cart</li>
-          <li><FontAwesomeIcon icon={faComment} /> Messages</li>
-          <li><FontAwesomeIcon icon={faBell} /> Notification</li>
-          <li><FontAwesomeIcon icon={faUser} /> Profile</li>
-          <li><FontAwesomeIcon icon={faPhone} /><a id = "anchor" href="#contact-us">Contact Us</a></li>
-        </ul>
-      </div>
-      <div className="main">
-        <div className="user-card">
-          <div className="user-profile">
-            <img src="https://via.placeholder.com/150" alt="Profile" />
-          </div>
-          <div className="user-info">
-            <h3>Rohit Tiwari</h3>
-            <p>Guru Jambheshwar University of Science & Technology</p>
-            <p>CSE(A&MI)</p>
-            <p>3rd Year contact</p>
-          </div>
-          <div className="user-actions">
-            <button className="chat-button">Chat</button>
-            <button className="edit-button">Edit</button>
-          </div>
-        </div>
-        <div className="products">
-          <div className="product">
+        <div className="main">
+          <Profile/>
+          <div className="products">
+           <div className="product">
             <img src="https://via.placeholder.com/150" alt="Product" />
             <h3>Maths Books</h3>
             <p>₹ 200</p>
@@ -85,15 +85,14 @@ function App() {
             <h3>Maths Books</h3>
             <p>₹ 200</p>
             <button className="edit-button">Edit</button>
+          </div>
           </div>
         </div>
-      </div>
-      
-    </div>
+        </div>
     <div id="contact-us">
       <ContactSection/></div>
-</div>
-  );
+      </div>
+        );
 }
 
 export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './Profile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCommentDots} from '@fortawesome/free-solid-svg-icons';
+import {faPen} from '@fortawesome/free-solid-svg-icons';
+
+function Profile (){
+const [isEditing, setIsEditing] = useState(false);
+  const [userProfile, setUserProfile] = useState({
+    name: 'Ankit Singh',
+    university: 'Guru Jambheshwar University of Science & Technology',
+    course: 'CSE(A&MI)',
+    year: '3rd Year contact',
+  });
+
+  function changeval() {
+    setIsEditing(true);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setUserProfile({ ...userProfile, [name]: value });
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+    return(
+<div>
+    <div className="user-card">
+            <div id='profilech'>         
+              <div className="user-profile">
+                <img src="https://via.placeholder.com/150" alt="Profile" />
+              </div>
+              <div className="user-info">
+                {isEditing ? (
+                  <form>
+                    <label>
+                      Name:
+                      <input type="text" name="name" value={userProfile.name} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                      University:
+                      <input type="text" name="university" value={userProfile.university} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                      Course:
+                      <input type="text" name="course" value={userProfile.course} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                      Year:
+                      <input type="text" name="year" value={userProfile.year} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <button onClick={handleSave}>Save</button>
+                  </form>
+                ) : (
+                  <>
+                    <h3>{userProfile.name}</h3>
+                    <p>{userProfile.university}</p>
+                    <p>{userProfile.course}</p>
+                    <p>{userProfile.year}</p>
+                  </>
+                )}
+              </div>
+            </div>
+
+            <div className="user-actions">
+              <button className="chat-button">Chat <FontAwesomeIcon icon={faCommentDots} /></button>
+              <button className="edit-button1" onClick={changeval}>Edit <FontAwesomeIcon icon={faPen} /></button>
+            </div>
+          </div>
+</div>
+    );
+}
+export default Profile;
